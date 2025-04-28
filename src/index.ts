d6d42855f67cfdacf62c936843bdb5000bf8eff7
seb <seb@sebs-MacBook-Air.local>
2025-04-28 19:49:34 +0200

    feat: add code

--- src/index.ts
+++ src/index.ts
@@ -13,11 +13,33 @@ export default {
         const webhooks = new Webhooks({
             secret: env.WEBHOOK_SECRET,
         });
-
         if (!(await webhooks.verify(body, signature))) {
             return new Response(null, { status: 401 });
         }
-
+        const payload = JSON.parse(body);
+        const res = await fetch(env.WEBHOOK_URL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                username: "VirtualStreets",
+                avatar_url: "https://avatars.githubusercontent.com/in/15368?v=4",
+                content: "<@1366451119525593158>",
+                embeds: [{
+                    color: 11027200,
+                    url: payload.head_commit.url,
+                    author: {
+                        name: "github-actions[bot]",
+                        icon_url: "https://avatars.githubusercontent.com/in/15368?v=4"
+                    },
+                    description: `[\`${payload.head_commit.id.substring(0, 7)}\`](${payload.head_commit.url}): ${new Date(payload.head_commit.timestamp).toLocaleString("en-GB")}`
+                }]
+            }),
+        });
+        if (!res.ok) {
+            return new Response(await res.text(), { status: 500 })
+        }
         return new Response(null, { status: 204 });
     },
 } satisfies ExportedHandler<Env>;
